e8a70fd540988bfd5228af78f3381670430ab3ec
Dr. Jochen Manns <manns@insynergie.de>
2020-11-01 12:24:27 +0100

    prepare migration to mbox 6

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,8 +1,11 @@
+import { configure } from 'mobx'
 import * as React from 'react'
 import { render } from 'react-dom'
 
 import { Root } from './root'
 
 import './index.scss'
+
+configure({ enforceActions: 'never' })
 
 render(<Root />, document.querySelector('body > div#app'))
